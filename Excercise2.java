705768a7f9fcd5cf5dde4ae5341e5fd37c284ce3
Suliman Alodaib <107861497+Suliman-7@users.noreply.github.com>
2024-07-02 13:30:50 +0300

    Add files via upload

---
+++ Excercise2.java
@@ -0,0 +1,137 @@
+
+import java.util.Random;
+import java.util.Scanner;
+
+
+    public class Excercise2 {
+        public static void main(String[] args) {
+            //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+            // to see how IntelliJ IDEA suggests fixing it.
+
+            Scanner input = new Scanner(System.in);
+
+
+            // Q1 :
+//
+//                System.out.println("Welcome enter your role :");
+//                String role = input.next();
+//
+//                if (role.equals("admin")){
+//                    System.out.println("Welcome admin");
+//                } else if (role.equals("superuser")) {
+//                    System.out.println("Welcome superuser");
+//                } else if (role.equals("user")) {
+//                    System.out.println("Welcome user");
+//                } else {
+//                    System.out.println("invalid role");
+//                }
+
+
+            // Q2
+
+//
+//                System.out.println("Please enter number 1 ");
+//                int num1 = input.nextInt();
+//                System.out.println("Please enter number 2 ");
+//                int num2 = input.nextInt();
+//                System.out.println("Please enter number 3 ");
+//                int num3 = input.nextInt();
+//
+//                if (num1 > num2 && num1 > num3) {
+//                    System.out.println("greatest :" + num1);
+//                }
+//                else if (num2 > num1 && num2 > num3) {
+//                    System.out.println("greatest :" + num2);
+//                }
+//                else if (num3 > num1 && num3 > num2) {
+//                    System.out.println("greatest :" + num3);
+//                }
+//                else {
+//                    System.out.println("all are equal");
+//                }
+
+
+            // Q3
+//
+//                Random random = new Random();
+//
+//                int day = random.nextInt(7);
+//
+//                switch (day) {
+//                case 1 : System.out.println("Sunday");
+//                break;
+//                case 2 : System.out.println("Monday");
+//                break;
+//                case 3 : System.out.println("Tuesday");
+//                break;
+//                case 4 : System.out.println("Wednesday");
+//                break;
+//                case 5 : System.out.println("Thursday");
+//                break;
+//                case 6 : System.out.println("Friday");
+//                break;
+//                case 7 : System.out.println("Saturday");
+//                break;
+//                }
+
+
+
+
+
+
+
+
+// Q4
+//                System.out.println("Please enter your numeric score ");
+//
+//                int score = input.nextInt();
+//
+//                if (score > 100) {
+//                    System.out.println("score should be less than 100");
+//                }
+//                else if (score >= 90 && score <= 100) {
+//                    System.out.println("numeric score : " + score);
+//                    System.out.println("Letter Grade : A");
+//                }
+//                else if (score >= 80 && score <= 89) {
+//                    System.out.println("numeric score : " + score);
+//                    System.out.println("Letter Grade : B");
+//                }
+//                else if (score >= 70 && score <= 79) {
+//                    System.out.println("numeric score : " + score);
+//                    System.out.println("Letter Grade : C");
+//                }
+//                else if (score >= 60 && score <= 69) {
+//                    System.out.println("numeric score : " + score);
+//                    System.out.println("Letter Grade : D");
+//                }
+//                else {
+//                    System.out.println("numeric score : " + score);
+//                    System.out.println("Letter Grade : F");
+//                }
+
+            // Q5
+//            System.out.println("Please enter your age :");
+//
+//            int age = input.nextInt();
+//
+//
+//            if (age < 13 ) {
+//                System.out.println("You are a Child");
+//            }
+//            else if (age >= 13 && age <= 19) {
+//                System.out.println("You are a Teenager");
+//            }
+//            else {
+//                System.out.println("You are an Adult");
+//            }
+//
+
+
+
+
+
+
+        }
+    }
+
